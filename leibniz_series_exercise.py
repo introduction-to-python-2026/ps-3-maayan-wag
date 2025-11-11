78906ddebbf2a70d03a46152fc453b09f96ae8b9
maayan-wag <maayan.wagshal@gmail.com>
2025-11-11 21:26:28 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,8 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+  l_series=[]
+  for n in range(n_terms):
+    term=((-1)**n)/(2*n+1)
+    l_series.append(term)
+  l_series_sum=sum(l_series)
+  l_series_sum*=4
+  return l_series_sum
