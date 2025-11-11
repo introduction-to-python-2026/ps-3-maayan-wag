e693fe0c37e7f6d4cf8f93e7324f8b36345d8b73
maayan-wag <maayan.wagshal@gmail.com>
2025-11-11 19:47:11 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -8,7 +8,7 @@ def move(my_list, direction):
   elif direction=='left':
     my_list[index_of_one]=0
     my_list[index_of_one-1]=1
-  elif direction=='right':
+  else:
     my_list[index_of_one]=0
     my_list[index_of_one+1]=1
   return my_list
